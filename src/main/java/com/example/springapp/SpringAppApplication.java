d5bc884747f2a57bf828bfc12bdc4222bc95ffd3
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-19 21:34:26 +0200

    Returning json objects from classes

--- src/main/java/com/example/springapp/SpringAppApplication.java
+++ src/main/java/com/example/springapp/SpringAppApplication.java
@@ -1,10 +1,12 @@
 package com.example.springapp;
 
+import com.example.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,7 +17,36 @@ public class SpringAppApplication {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
      @GetMapping("/")
-	 public static List< String> hello(){
-		return List.of("Hello" , "World");
+	 public static List<Student> hello(){
+		return List.of(
+				new Student(
+						1L ,
+						"Jazzy Bruno",
+						"jazzybruno45@gmail.com",
+						LocalDate.of(2006 , 04 , 25),
+						21
+				),
+		 new Student(
+				 1L ,
+				 "Jazzy Bruno",
+				 "jazzybruno45@gmail.com",
+				 LocalDate.of(2006 , 04 , 25),
+				 21
+		 ),
+		 new Student(
+				 1L ,
+				 "Jazzy Bruno",
+				 "jazzybruno45@gmail.com",
+				 LocalDate.of(2006 , 04 , 25),
+				 21
+		 ),
+		 new Student(
+				 1L ,
+				 "Jazzy Bruno",
+				 "jazzybruno45@gmail.com",
+				 LocalDate.of(2006 , 04 , 25),
+				 21
+		 )
+		);
 	}
 }
