10410eff4d3a3a7dfede878404cdc949c1cb8167
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-02-07 06:37:29 +0200

    Added the message class

--- src/main/java/com/example/springapp/classes/Message.java
+++ src/main/java/com/example/springapp/classes/Message.java
@@ -1,4 +1,52 @@
 package com.example.springapp.classes;
 
+import java.time.LocalDate;
+
 public class Message {
+    private Long message_id;
+    private LocalDate TimeSent;
+    private User sender;
+    private User receiver;
+
+    public Message(){
+
+    }
+
+    public Message(LocalDate timeSent, User sender, User receiver) {
+        TimeSent = timeSent;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public Long getMessage_id() {
+        return message_id;
+    }
+
+    public void setMessage_id(Long message_id) {
+        this.message_id = message_id;
+    }
+
+    public LocalDate getTimeSent() {
+        return TimeSent;
+    }
+
+    public void setTimeSent(LocalDate timeSent) {
+        TimeSent = timeSent;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
 }
